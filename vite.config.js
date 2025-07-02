431752b7570a1d95430d02e9222638d73efd36a2
GadflyR <ottomanhabsburg@gmail.com>
2025-07-02 23:38:50 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/CognitiveLab/',
   resolve: {
     alias: {
       // ① any import that starts with "@/…" now maps to /src/…
